14b4b9c7410b57d47393dcafc9bb20eceef7fd51
Kelvin Lee <kiyolee@gmail.com>
2018-06-02 23:32:14 +1000

    Updated to version 8.42.

--- win32/pcre_version.h
+++ win32/pcre_version.h
@@ -1,4 +1,4 @@
-#define PCRE_VERSION_STR   "8.39"
+#define PCRE_VERSION_STR   "8.42"
 #define PCRE_VER_MAJOR     8
-#define PCRE_VER_MINOR     39
+#define PCRE_VER_MINOR     42
 #define PCRE_VER_REVISION  0
