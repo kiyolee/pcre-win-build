efe66ea0e5b59dfbc1e8609ff0608737024e275f
Kelvin Lee <kiyolee@gmail.com>
2019-03-16 21:50:51 +1100

    Update to 8.43.

--- win32/pcre_version.h
+++ win32/pcre_version.h
@@ -1,4 +1,4 @@
-#define PCRE_VERSION_STR   "8.42"
+#define PCRE_VERSION_STR   "8.43"
 #define PCRE_VER_MAJOR     8
-#define PCRE_VER_MINOR     42
+#define PCRE_VER_MINOR     43
 #define PCRE_VER_REVISION  0
